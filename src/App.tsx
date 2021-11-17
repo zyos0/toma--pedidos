960fb3aff04ef29c8efbeabb367b7664a966a52c
roberto.malaver <roberto.malaver@cision.com>
2021-11-16 19:22:48 -0500

    create-react-app-react-hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,101 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+interface ButtonProps {
+    text?: string;
+    handleOnClick: () => void;
+}
+
+const Button: React.FC<ButtonProps> = ({ text, handleOnClick, children }) => {
+    return <button onClick={handleOnClick}>{children || text}</button>;
+};
+
+interface ItemProps {
+    label: string;
+    handleDelete: (item: string) => void;
+}
+
+const Item: React.FC<ItemProps> = ({ label, handleDelete }) => {
+    useEffect(() => {
+        const timerId = setInterval(() => {
+            console.log(`${label} salutes you!`);
+        }, 2000);
+
+        return () => window.clearInterval(timerId);
+    }, [label]);
+
+    return (
+        <div
+            style={{
+                display: 'flex',
+                justifyContent: 'space-evenly',
+                width: 350,
+                margin: '0 auto',
+            }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            <span>{label}</span>
+            <Button handleOnClick={() => handleDelete(label)}>Remove</Button>
+        </div>
+    );
+};
+
+const ListingPage = () => {
+    const blackList = ['ana', 'julia', 'juan'];
+
+    const [name, setName] = useState('Maria');
+
+    const [list, updateList] = useState<string[]>([]);
+
+    useEffect(() => {
+        if (blackList.some((entry) => entry === name)) {
+            setName('');
+        }
+    });
+
+    const handleUpdateList = (newEntry: string) => {
+        const newList = list.concat(newEntry); // [...list, newEntry]
+        setName('');
+        updateList(newList);
+    };
+
+    const handleRemoveFromList = (target: string) => {
+        const newList = list.filter((item) => item !== target);
+        updateList(newList);
+    };
+
+    const handleNameUpdate = (evt: any) => {
+        setName(evt.target.value);
+    };
+
+    const renderListEntry = (entry: string) => {
+        return (
+            <Item
+                key={entry}
+                label={entry}
+                handleDelete={handleRemoveFromList}
+            />
+        );
+    };
+
+    return (
+        <div>
+            <input type="text" value={name} onChange={handleNameUpdate} />
+            <Button
+                text="Add new Entry"
+                handleOnClick={() => handleUpdateList(name)}
+            />
+
+            <div>{list.map(renderListEntry)}</div>
+        </div>
+    );
+};
+
+function App() {
+    return (
+        <div className="App">
+            <ListingPage />
+        </div>
+    );
 }
 
 export default App;
